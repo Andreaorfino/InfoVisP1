04d239cf0b40ff286c4a5b1dff11f2cf76f39a7c
Andreaorfino <sharswood.ap@gmail.com>
2020-05-26 18:07:27 +0200

    commenti e descrizione

--- script/d3-farfalle.js
+++ script/d3-farfalle.js
@@ -13,10 +13,7 @@ function getRandomColor() {
     return color + "3";
 }
 
-d3.selectAll("p").style("color", "green");
-
-
-
+// ----------------------------
 
 var width = window.innerWidth - 10;
 var height = window.innerHeight - 100;
@@ -24,38 +21,20 @@ var height = window.innerHeight - 100;
 var x = width / 2
 var y = height / 2
 
-
 var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-
-
-
-
- for (var i = 0; i < 10; i++) {
-
-    svg.append("path").attr("class",'farfalla' + i  ).attr("style","stroke: black; display: inline;").attr("d", "M264,253 C267,217,226,175,245,190C265,206,345,278,369,300C394,323,363,296,372,263C382,231,431,193,404,206C377,219,274,283,245,301C216,318,262,288,264,253")
-    .style("fill", getRandomColor())
-    .attr("transform", "scale(0.3)" + "translate("+getRandomInt(width)+"," + getRandomInt(height) + ")")
-    
-
-    
-
-/*     svg.append("circle")
-        .attr("class", "farfalla" + i)
+// crea le farfalle  e le posiziona casualmente
+for (var i = 0; i < 10; i++) {
+    svg.append("path").attr("class", 'farfalla' + i).attr("style", "stroke: black; display: inline;").attr("d", "M264,253 C267,217,226,175,245,190C265,206,345,278,369,300C394,323,363,296,372,263C382,231,431,193,404,206C377,219,274,283,245,301C216,318,262,288,264,253")
         .style("fill", getRandomColor())
-        .attr("cx", getRandomInt(width))
-        .attr("cy", getRandomInt(height))
-        .attr("r", 20)
-        .attr("cx", x + data[j][i].x)
-        .attr("cy", y + (-1 * data[j][i].y))
- */
-} 
+        .attr("transform", "scale(0.3)" + "translate(" + getRandomInt(width) + "," + getRandomInt(height) + ")")
+}
 
+// fa apparire la scritta ciao
 function drawCiao(data) {
-    console.log(data);
 
     for (var j = 0; j < data.length; j++) {
         for (var i = 0; i < 10; i++) {
@@ -65,16 +44,12 @@ function drawCiao(data) {
                 .duration(1000)
                 .delay(2000 * j)
                 .style("fill", getRandomColor())
-                .attr("transform", "scale(0.3)" + "translate("+ (3*(x + data[j][i].x)-200) +"," + (3*(y + (-1 * data[j][i].y))-400) + ")")
-
+                .attr("transform", "scale(0.3)" + "translate(" + (3 * (x + data[j][i].x) - 200) + "," + (3 * (y + (-1 * data[j][i].y)) - 400) + ")")
         }
-
-
-
     }
-
 }
 
+// Event Listener che attende la pressione della 'c'
 d3.select("body")
     .on("keydown", e => {
         if (d3.event.keyCode == 67) {
@@ -83,6 +58,3 @@ d3.select("body")
             });
         }
     });
-
-
-
